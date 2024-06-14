b4f2941502d5ce90b7644ce2d64a2209d926eee3
Clare S <Clare.Stock@gmail.com>
2024-06-13 22:25:31 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule, DecimalPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ToDo } from './models/to-do';
+import { FormsModule } from '@angular/forms';
+import { find } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, DecimalPipe, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title:string = 'To Do List';
+
+myList:ToDo[] = [
+  {
+    task: "Exercise",
+    completed: false,
+    duration: .5,
+    priority: 1
+  },
+  {
+    task: "Relax Reading",
+    completed: false,
+    duration: 1,
+    priority: 3
+  },
+  {
+    task: "Walk Dog",
+    completed: true,
+    duration: .5,
+    priority: 2
+  },
+  {
+    task: "Mow Lawn",
+    completed:false,
+    duration: 3,
+    priority: 3
+  }
+];
+
+deleteTask(targetTask:ToDo):void{
+  let index: number = this.myList.findIndex(i => i == targetTask)
+}
+
+showPriority(num:number):string{
+  if(num == 3){
+
+    return "Low Priority ↓ "
+  }
+  else if(num == 2){
+    return ""
+  }
+  else if(num == 1){
+    return " High Priority ↑ "
+  }
+  return ""
+}
+
+formTask: ToDo = {} as ToDo
+
+completeTask(targetTask:ToDo):void{
+  if(targetTask.completed == true){
+    targetTask.completed = false
+  }
+  else{
+    targetTask.completed = true
+  }
+}
+
+addTask():void{
+  let newTask: ToDo = { ...this.formTask}, completed: false;
+  this.myList.push(newTask)
+
+}
+
+}
+
+
